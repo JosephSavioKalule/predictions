cf7c11467af2450524b77201a0965247edba018a
Joseph Kalule <joseph.savio.kalule@gmail.com>
2018-08-11 11:21:13 +0000

    Order matches by date and don't show older matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -6,7 +6,7 @@ class MatchesController < ApplicationController
   
   def index
     @league = League.find(params[:league_id])
-    @matches = @league.matches
+    @matches = @league.matches.where("match_date_time > ?", 1.day.ago).order(:match_date_time)
   end
   
   def show
