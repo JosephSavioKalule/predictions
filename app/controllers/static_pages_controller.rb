e5fc00b11578b19fa714a85035980a7a98b951dd
Joseph Kalule <joseph.savio.kalule@gmail.com>
2018-08-27 11:48:33 +0000

    Set upcoming matches variable even when not logged in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     @leagues = League.all.order(:id)
+    @upcoming_matches = Match.where("match_date_time < ? and match_date_time > ?", 3.days.from_now, 30.seconds.from_now)
     if logged_in?
       @user = current_user
-      @upcoming_matches = Match.where("match_date_time < ? and match_date_time > ?", 3.days.from_now, 30.seconds.from_now)
     end
   end
 
