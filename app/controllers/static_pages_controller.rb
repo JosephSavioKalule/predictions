76182bbaf35722b65ffb85f8b7f66aba68d0aa99
Joseph Kalule <joseph.savio.kalule@gmail.com>
2018-08-27 10:42:40 +0000

    Add upcoming matches to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
     @leagues = League.all.order(:id)
     if logged_in?
       @user = current_user
+      @upcoming_matches = Match.where("match_date_time < ? and match_date_time > ?", 3.days.from_now, 30.seconds.from_now)
     end
   end
 
