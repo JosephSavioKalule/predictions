29aaf2aa7336234a195aab74bf1b8d845f3903f9
Joseph Kalule <joseph.savio.kalule@gmail.com>
2018-09-14 10:48:16 +0000

    Order by time and limit number of upcoming matches

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     @leagues = League.all.order(:id)
-    @upcoming_matches = Match.where("match_date_time < ? and match_date_time > ?", 3.days.from_now, 30.seconds.from_now)
+    @upcoming_matches = Match.where("match_date_time < ? and match_date_time > ?",
+                        3.days.from_now, 30.seconds.from_now).order(:match_date_time).limit(10)
     if logged_in?
       @user = current_user
     end
