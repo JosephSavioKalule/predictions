9bfa5e071570cbab3db6c1de2e665a756a7329d8
Joseph Kalule <joseph.savio.kalule@gmail.com>
2018-10-08 15:50:59 +0000

    Show team's recent form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,5 +6,27 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find(params[:id])
     @matches = Match.where("home_team_id=? or away_team_id=?", @team.id, @team.id).limit(5).order(match_date_time: :desc)
+    @form = []
+    @matches.each do |m|
+      if m.home_goals && m.away_goals
+        if m.home_team_id == @team.id # Home game
+          if m.home_goals > m.away_goals
+            @form.push "W"
+          elsif m.home_goals == m.away_goals
+            @form.push "D"
+          else
+            @form.push "L"
+          end
+        else                          # Away game
+          if m.home_goals > m.away_goals
+            @form.push "L"
+          elsif m.home_goals == m.away_goals
+            @form.push "D"
+          else
+            @form.push "W"
+          end
+        end
+      end
+    end
   end
 end
