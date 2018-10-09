81f97b2541923fd3528d4c2ea05338bcc2e0033b
Joseph Kalule <joseph.savio.kalule@gmail.com>
2018-10-09 10:54:48 +0000

    Change order of matches for team form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,7 +7,7 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
     @matches = Match.where("home_team_id=? or away_team_id=?", @team.id, @team.id).limit(5).order(match_date_time: :desc)
     @form = []
-    @matches.each do |m|
+    @matches.reverse.each do |m|
       if m.home_goals && m.away_goals
         if m.home_team_id == @team.id # Home game
           if m.home_goals > m.away_goals
