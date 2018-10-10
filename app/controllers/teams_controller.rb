e414fbc96dc20fa381eed16e0a2c4cfa2eaae24c
Joseph Kalule <joseph.savio.kalule@gmail.com>
2018-10-10 10:56:35 +0000

    Move matches and form code into model

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,28 +5,7 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
-    @matches = Match.where("home_team_id=? or away_team_id=?", @team.id, @team.id).limit(5).order(match_date_time: :desc)
-    @form = []
-    @matches.reverse.each do |m|
-      if m.home_goals && m.away_goals
-        if m.home_team_id == @team.id # Home game
-          if m.home_goals > m.away_goals
-            @form.push "W"
-          elsif m.home_goals == m.away_goals
-            @form.push "D"
-          else
-            @form.push "L"
-          end
-        else                          # Away game
-          if m.home_goals > m.away_goals
-            @form.push "L"
-          elsif m.home_goals == m.away_goals
-            @form.push "D"
-          else
-            @form.push "W"
-          end
-        end
-      end
-    end
+    @matches = @team.matches
+    @form = @team.form(@matches)
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,33 @@
 class Team < ApplicationRecord
   belongs_to :league
+  
+  def matches
+    @matches = Match.where("home_team_id=? or away_team_id=?", self.id, self.id).limit(5).order(match_date_time: :desc)
+  end
+  
+  def form(matches)
+    @form = []
+    matches.reverse.each do |m|
+      if m.home_goals && m.away_goals
+        if m.home_team_id == self.id # Home game
+          if m.home_goals > m.away_goals
+            @form.push "W"
+          elsif m.home_goals == m.away_goals
+            @form.push "D"
+          else
+            @form.push "L"
+          end
+        else                          # Away game
+          if m.home_goals > m.away_goals
+            @form.push "L"
+          elsif m.home_goals == m.away_goals
+            @form.push "D"
+          else
+            @form.push "W"
+          end
+        end
+      end
+    end
+    @form
+  end
 end
