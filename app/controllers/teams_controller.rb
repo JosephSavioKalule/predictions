a73b4caf25b012f40830d0f8b90c05f40f0c6234
Joseph Kalule <joseph.kalule@valiantys.com>
2021-07-20 23:22:52 +0200

    Select teams from most recent season, ordered by name

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,9 +2,10 @@ class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update]
   before_action :logged_in_user, only: [:edit, :update]
   before_action :admin_user, only: [:edit, :update]
-  
+
   def index
-    @teams = Team.where("league_id>4").order(:name)
+    @teams = Team.where("league_id in (?)",
+           League.where("season_id=?",Season.last.id).pluck(:id)).order(:name)
   end
 
   def show
@@ -12,10 +13,10 @@ class TeamsController < ApplicationController
     @matches = @team.past_matches
     @form = @team.form(@matches)
   end
-  
+
   def edit
   end
-  
+
   def update
     if @team.update_attributes(team_update_params)
       flash[:success] = "Team updated"
@@ -24,13 +25,13 @@ class TeamsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
-  
+
     def set_team
       @team = Team.find(params[:id])
     end
-    
+
     def team_update_params
       params.require(:team).permit(:name, :logo_url)
     end
