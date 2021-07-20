fa4b1a56bf7478739e9fc51914a1b3344d45c955
Joseph Kalule <joseph.kalule@valiantys.com>
2021-07-20 13:23:25 +0200

    Dynamically select leagues belonging to most recent season

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,9 +1,10 @@
 class LeaguesController < ApplicationController
   before_action :logged_in_user, only: [:rankings, :edit, :update]
   before_action :admin_user, only: [:edit, :update]
-  
+
   def index
-    @leagues = Season.find(2).leagues
+    @season = Season.last
+    @leagues = @season.leagues
   end
 
   def show
@@ -11,11 +12,11 @@ class LeaguesController < ApplicationController
     @teams = @league.teams.order(:name)
     @recent_matches = @league.matches.where("match_date_time < ?", 1.minute.ago).order(match_date_time: :desc).limit(5)
   end
-  
+
   def edit
     @league = League.find(params[:id])
   end
-  
+
   def update
     @league = League.find(params[:id])
     if @league.update(league_update_params)
@@ -25,17 +26,17 @@ class LeaguesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def table
   end
-  
+
   def rankings
     @league = League.find(params[:id])
     @predictors = @league.predictors.order(points: :desc).paginate(page: params[:page])
   end
-  
+
   private
-  
+
     def league_update_params
       params.require(:league).permit(:name, :logo_url)
     end
