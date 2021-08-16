a1ebd864a8ed51206b7f5598815b64f5393c9abb
Joseph Kalule <joseph.kalule@valiantys.com>
2021-08-16 20:05:43 +0200

    Dynamically create predictor for current season

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,15 +1,16 @@
 class AccountActivationsController < ApplicationController
-  
+
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
       flash[:success] = "Account activated!"
-      user.predictors.build(league_id:1).save!
-      user.predictors.build(league_id:2).save!
-      user.predictors.build(league_id:3).save!
-      user.predictors.build(league_id:4).save!
+      # get league ids for current season
+      @league_ids = League.where(season_id: Season.where(start_year: Date.today.year).pluck(:id)).pluck(:id)
+      @league_ids.each do |lg_id|
+        user.predictors.build(league_id: lg_id).save!
+      end
       SettingsBox.create!(user_id: user.id, receive_email: true)
       redirect_to user
     else
