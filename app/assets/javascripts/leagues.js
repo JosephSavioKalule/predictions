b8eeba28b656f19664bede37c73ac95a421548dc
Joseph Kalule <joseph.kalule@valiantys.com>
2021-08-19 21:34:29 +0200

    Add link to team from match page

--- app/assets/javascripts/leagues.js
+++ app/assets/javascripts/leagues.js
@@ -2,4 +2,7 @@ $( document ).on('turbolinks:load', function() {
   $("tr[data-link]").click(function() {
     window.location = $(this).data("link")
   })
+  $("span[data-link]").click(function() {
+    window.location = $(this).data("link")
+  })
 })
