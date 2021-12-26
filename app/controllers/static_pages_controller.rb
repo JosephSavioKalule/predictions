1192945f69b3a6642518df4367b4b5eac53c72a0
Joseph Kalule <joseph.savio.kalule@gmail.com>
2021-12-26 16:18:05 +0100

    Group upcoming matches by day

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,14 @@ class StaticPagesController < ApplicationController
   def home
     @season = Season.last
     @leagues = @season.leagues.order(:id)
+    d = DateTime.now
     @upcoming_matches = Match.where("match_date_time < ? and match_date_time > ?",
                         3.days.from_now, 30.seconds.from_now).order(:match_date_time).limit(10)
+    @upcoming_today = Match.where("match_date_time <? and match_date_time > ?",
+                                  (24-d.hour).hours.from_now, 30.seconds.from_now)
+    @upcoming_tomor = Match.where("match_date_time <? and match_date_time > ?",
+                                  (48-d.hour).hours.from_now, (24-d.hour).hours.from_now)
+    @upcoming_later = Match.where("match_date_time > ?", (48-d.hour).hours.from_now)
     if logged_in?
       @user = current_user
     end
